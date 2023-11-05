34c89a622d8b7a9157225ec76e6d1957a1597965
01AT <36324854+01AT@users.noreply.github.com>
2023-11-05 21:53:47 +0530

    Update dockerimagepush.groovy

--- vars/dockerimagepush.groovy
+++ vars/dockerimagepush.groovy
@@ -1,22 +1,22 @@
-// def call(String project, String ImageTag, String hubUser){
-//     withCredentials([usernamePassword(
-//             credentialsId: "docker",
-//             usernameVariable: "USER",
-//             passwordVariable: "PASS"
-//     )]) {
-//         sh "docker login -u '$USER' -p '$PASS'"
-//     }
-//     sh "docker image push ${hubUser}/${project}:${ImageTag}"
-//     sh "docker image push ${hubUser}/${project}:latest"   
-// }
-
-
-def call(String aws_account_id, String region, String ecr_repoName){
-    
-    sh '''
-     docker login -u AWS -p $(aws ecr get-login-password --region ''' + region + ''') ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-     // docker login -u AWS -p ${aws ecr get-login-password --region ${region}} ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-     // aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-     docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
-    '''
+def call(String project, String ImageTag, String hubUser){
+    withCredentials([usernamePassword(
+            credentialsId: "docker",
+            usernameVariable: "USER",
+            passwordVariable: "PASS"
+    )]) {
+        sh "docker login -u '$USER' -p '$PASS'"
+    }
+    sh "docker image push ${hubUser}/${project}:${ImageTag}"
+    sh "docker image push ${hubUser}/${project}:latest"   
 }
+
+
+// def call(String aws_account_id, String region, String ecr_repoName){
+    
+//     sh '''
+//      docker login -u AWS -p $(aws ecr get-login-password --region ''' + region + ''') ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+//      // docker login -u AWS -p ${aws ecr get-login-password --region ${region}} ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+//      // aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+//      docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+//     '''
+// }
